0812d294f1da496091395e67e91af6e199cc97e8
anushreebose29 <anushreebose29@gmail.com>
2022-01-25 17:15:26 +0100

    initial setup

---
+++ src/test/java/Test99GuruLoginWithPageFactory.java
@@ -0,0 +1,67 @@
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.testng.Assert;
+
+import org.testng.annotations.BeforeTest;
+
+import org.testng.annotations.Test;
+
+
+public class Test99GuruLoginWithPageFactory {
+
+
+    WebDriver driver;
+    Guru99Login objLogin;
+    Guru99HomePage objHomePage;
+
+    @BeforeTest
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://demo.guru99.com/V4/");
+
+    }
+
+    /**
+
+     * This test go to http://demo.guru99.com/V4/
+
+     * Verify login page title as guru99 bank
+
+     * Login to application
+
+     * Verify the home page using Dashboard message
+
+     */
+
+    @Test(priority=0)
+
+    public void test_Home_Page_Appear_Correct(){
+
+        //Create Login Page object
+
+        objLogin = new Guru99Login(driver);
+        //Verify login page title
+        String loginPageTitle = objLogin.getLoginTitle();
+        Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+
+        //login to application
+        objLogin.loginToGuru99("mgr123", "mgr!23");
+        // go the next page
+
+        objHomePage = new Guru99HomePage(driver);
+        //Verify home page
+        Assert.assertTrue(objHomePage.getHomePageDashboardUserName().toLowerCase().contains("manger id : mgr123"));
+
+    }
+
+}
